6a59e6ed20232466eca226ee60ddc7bbf14cca4a
Thushara Thiwanka <thusharathiwanka.dev@gmail.com>
2022-08-03 17:12:17 +0530

    CREATE: input controls added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,56 @@ import { useContext } from "react";
 import { SalaryContext } from "../contexts/SalaryContext";
 
 const Form = () => {
-	const { salary, setSalary } = useContext(SalaryContext);
+	const { salary, setSalary, earnings, setEarnings, deductions, setDeductions } =
+		useContext(SalaryContext);
+
+	const handleEarningsChange = (e, index) => {
+		const newEarnings = [...earnings];
+		newEarnings[index].amount = e.target.value;
+		setEarnings(newEarnings);
+		console.log(e.target.value);
+		console.log(earnings);
+	};
+
+	const handleEpfEtfChange = index => {
+		const newEarnings = [...earnings];
+		newEarnings[index].epfEtf = !newEarnings[index].epfEtf;
+		setEarnings(newEarnings);
+		console.log(earnings);
+	};
+
+	const handleDeductionsChange = (e, index) => {
+		const newDeductions = [...deductions];
+		newDeductions[index].amount = e.target.value;
+		setDeductions(newDeductions);
+		console.log(deductions);
+	};
+
+	const addEarningRow = e => {
+		e.preventDefault();
+		const newRow = { amount: "", epfEtf: false };
+		setEarnings([...earnings, newRow]);
+	};
+
+	const removeEarningRow = (e, index) => {
+		e.preventDefault();
+		const newEarnings = [...earnings];
+		newEarnings.splice(index, 1);
+		setEarnings(newEarnings);
+	};
+
+	const addDeductionRow = e => {
+		e.preventDefault();
+		const newRow = { amount: "" };
+		setDeductions([...deductions, newRow]);
+	};
+
+	const removeDeductionRow = (e, index) => {
+		e.preventDefault();
+		const newDeductions = [...deductions];
+		newDeductions.splice(index, 1);
+		setDeductions(newDeductions);
+	};
 
 	return (
 		<form className="form border">
@@ -28,17 +77,31 @@ const Form = () => {
 			<div className="earnings">
 				<h5 className="body-text-semibold padding-top">Earnings</h5>
 				<p className="body-text-small">Allowance, Fixed Allowance, Bonus and etc.</p>
-				<div className="input-container">
-					<input type="number" name="earning" id="earning" className="body-text" />
-					<button className="remove-button button">
-						<img src="/assets/remove.png" alt="remove-icon" />
-					</button>
-					<div className="flex-center checkbox-container">
-						<input type="checkbox" id="epf-etf" name="epf-etf" />
-						<label htmlFor="epf-etf">EPF/ETF</label>
+				{earnings.map((earning, index) => (
+					<div className="input-container" key={index}>
+						<input
+							type="number"
+							name="amount"
+							className="body-text"
+							value={earning.amount}
+							onChange={e => handleEarningsChange(e, index)}
+						/>
+						<button className="remove-button button" onClick={e => removeEarningRow(e, index)}>
+							<img src="/assets/remove.png" alt="remove-icon" />
+						</button>
+						<div className="flex-center checkbox-container">
+							<input
+								type="checkbox"
+								name="epfEtf"
+								value={earning.isIncluded}
+								checked={earning.isIncluded}
+								onChange={() => handleEpfEtfChange(index)}
+							/>
+							<label htmlFor="epf-etf">EPF/ETF</label>
+						</div>
 					</div>
-				</div>
-				<button className="flex-between button">
+				))}
+				<button className="flex-between button" onClick={e => addEarningRow(e)}>
 					<img src="/assets/plus.png" alt="plus-icon" className="button-img" />
 					Add New Allowance
 				</button>
@@ -47,13 +110,22 @@ const Form = () => {
 			<div className="deductions">
 				<h5 className="body-text-semibold padding-top">Deductions</h5>
 				<p className="body-text-small">Salary Advances, Loan Deductions and all</p>
-				<div className="input-container">
-					<input type="number" name="deduction" id="deduction" className="body-text" />
-					<button className="remove-button button">
-						<img src="/assets/remove.png" alt="remove-icon" />
-					</button>
-				</div>
-				<button className="flex-between button">
+				{deductions.map((deduction, index) => (
+					<div className="input-container" key={index}>
+						<input
+							type="number"
+							name="deduction"
+							id="deduction"
+							className="body-text"
+							value={deduction.amount}
+							onChange={e => handleDeductionsChange(e, index)}
+						/>
+						<button className="remove-button button" onClick={e => removeDeductionRow(e, index)}>
+							<img src="/assets/remove.png" alt="remove-icon" />
+						</button>
+					</div>
+				))}
+				<button className="flex-between button" onClick={e => addDeductionRow(e)}>
 					<img src="/assets/plus.png" alt="plus-icon" className="button-img" />
 					Add New Deduction
 				</button>
--- src/contexts/SalaryContext.jsx
+++ src/contexts/SalaryContext.jsx
@@ -1,20 +1,20 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState } from "react";
 
 export const SalaryContext = createContext();
 
 const SalaryContextProvider = ({ children }) => {
 	const [salary, setSalary] = useState({
-		basicSalary: 0,
-		grossEarning: 0,
-		grossDeduction: 0,
-		netSalary: 0,
-		employeeEPF: 0,
-		employerEPF: 0,
-		employerETF: 0,
-		costToCompany: 0,
+		basicSalary: "",
+		grossEarning: "",
+		grossDeduction: "",
+		netSalary: "",
+		employeeEPF: "",
+		employerEPF: "",
+		employerETF: "",
+		costToCompany: "",
 	});
-	const [earnings, setEarnings] = useState([{ amount: null, isIncluded: false }]);
-	const [deductions, setDeductions] = useState([{ amount: 0 }]);
+	const [earnings, setEarnings] = useState([{ amount: "", epfEtf: false }]);
+	const [deductions, setDeductions] = useState([{ amount: "" }]);
 
 	return (
 		<SalaryContext.Provider
